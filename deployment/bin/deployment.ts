8d832dcb18a8598f93c83a51e2c31b8d4a68aed2
Leejjon <leejjon@gmail.com>
2022-05-25 09:36:27 +0200

    Change default branch to cdk branch.

--- deployment/bin/deployment.ts
+++ deployment/bin/deployment.ts
@@ -5,11 +5,11 @@ import {PipelineStack} from "../lib/pipeline-stack";
 
 const app = new cdk.App();
 
-new PipelineStack(app, "mijn-omgeving-frontend-pipeline", {
+new PipelineStack(app, "basic-auth-bug-pipeline", {
     codestarArn:
         "arn:aws:codestar-connections:eu-central-1:039085306114:connection/010d9616-2ce5-4d82-a61d-75938f7b0deb",
     pipelineName: "basic-auth-bug-pipeline",
     repositoryName: "leejjon/basic-auth-bug",
-    branchName: "main",
+    branchName: "addcdk",
     accountId: "",
 });
