73790dc004ab0a1e9324c330f37d26b96116aa20
Leejjon <leejjon@gmail.com>
2022-05-25 09:32:46 +0200

    Add codestar connection arn

--- deployment/bin/deployment.ts
+++ deployment/bin/deployment.ts
@@ -7,7 +7,7 @@ const app = new cdk.App();
 
 new PipelineStack(app, "mijn-omgeving-frontend-pipeline", {
     codestarArn:
-        "codestararn",
+        "arn:aws:codestar-connections:eu-central-1:039085306114:connection/010d9616-2ce5-4d82-a61d-75938f7b0deb",
     pipelineName: "basic-auth-bug-pipeline",
     repositoryName: "leejjon/basic-auth-bug",
     branchName: "main",
